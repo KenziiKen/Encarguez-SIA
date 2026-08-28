92bb4f23c21d6f4c079767a75bc96c1dcfb98e8c
unknown <pc15@csitlab.local>
2026-08-28 08:22:20 +0800

    feat: express server setup

---
+++ index.js
@@ -0,0 +1,25 @@
+//package imports
+const express = require('express');
+
+//server setup
+const server = express()
+const PORT = 2607
+const HOSTNAME ='0.0.0.0'
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is running: ${HOSTNAME}:${PORT}`);
+})
+
+let data = [
+    {
+        id: 1,
+        name: 'Ken',
+    },
+    {
+        id: 2,
+        name: 'Ben',
+    },
+    {
+        id: 3,
+        name: 'Zen',
+    },
+]
