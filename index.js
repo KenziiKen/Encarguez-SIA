ae6b0523db6651b1eef8a812bf73f7ef9c9e5cd9
ken shinn <kenshinnencarguez@gmail.com>
2026-09-13 17:27:28 +0800

    fix: port num

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 //server setup
 
 const server = express()
-const PORT = 3000
+const PORT = 2607
 // const HOSTNAME ='0.0.0.0'
 
 // server.listen(PORT, HOSTNAME, () => {
